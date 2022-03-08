627c98c9585c8056734248d4137ac79e48f42926
Ethan Kinnear <51250849+superatomic@users.noreply.github.com>
2022-03-07 19:47:53 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Spider Drawing with Python's `turtle`
+This is a very basic script to draw a spider with Python's [`turtle`](https://docs.python.org/3/library/turtle.html) graphics module.
+
+The rendered drawing is used as [my profile](https://github.com/superatomic) image.
+
+## Output
+
+![A spider](spider.png)  <!-- Their name is Billie -->
+
+## Installation
+
+### Download the source
+Pick any method to install the source files or just [download the source files as a ZIP](https://github.com/superatomic/spider/archive/refs/heads/main.zip).
+
+- Git
+  ```shell
+  git clone https://github.com/superatomic/spider.git
+  ```
+- Github CLI
+  ```shell
+  gh repo clone superatomic/spider
+  ```
+  
+### Running the program
+
+Simply run the program with `python3 main.py` to draw the spider.
+This program has been verified to work on Python 3.7+, and may or may not work on lower versions.
+
+<!-- Thanks to https://stackoverflow.com/questions/58737436/ -->
+| :heavy_exclamation_mark: | Your Python installation must support [`tkinter`](https://docs.python.org/3/library/tkinter.html) for the program to run! |
+|--------------------------|:--------------------------------------------------------------------------------------------------------------------------|
+
+## That's it!
+
+Hopefully this program is useful to somebody someday. I certainly had fun writing it.
+
---
+++ main.py
@@ -0,0 +1,77 @@
+"""
+Draws a spider using tkinter (via turtle).
+"""
+__author__ = 'Ethan Kinnear - [superatomic](https://github.com/superatomic)'
+
+import turtle
+from math import tau  # 0.5 * tau = pi (https://tauday.com/tau-manifesto)
+
+SCALE = 20
+
+
+BODY_SIZE = 4 * SCALE
+LEG_THICKNESS = SCALE // 2  # 5
+LEG_LENGTH = 7 * SCALE
+
+
+# Define the layout of the spider.
+# `L` is leg, `E` is eye, and ` ` is nothing.
+# noinspection SpellCheckingInspection
+TURTLE_BODY_PARTS = r'LLLL LLLL' 'EE'
+
+
+def draw_spider(pen: turtle.RawPen) -> None:
+
+    # Draw the spider's body
+    pen.dot(BODY_SIZE * 2)
+
+    # Draw the spider's legs and eyes
+    pen.pensize(LEG_THICKNESS)
+    for index, part in enumerate(TURTLE_BODY_PARTS):
+        pen.up()
+        pen.goto(0, 0)
+        pen.setheading(tau * index / len(TURTLE_BODY_PARTS))
+
+        draw_body_part(part, pen)
+
+
+def draw_body_part(part, pen: turtle.RawPen) -> None:
+    """
+    Draws a single body part.
+    :param part: Either 'L', 'E', or ' '.
+    :param pen: The turtle.
+    """
+    if part == 'L':  # Leg
+        pen.pencolor('black')
+        pen.down()
+        pen.forward(LEG_LENGTH)
+    elif part == 'E':  # Eye
+        pen.forward(BODY_SIZE * 2 / 3)
+        pen.down()
+        pen.dot(3 * LEG_THICKNESS - 1, 'white')
+        pen.dot(LEG_THICKNESS, 'black')
+    elif part != ' ':  # Nothing
+        raise ValueError("TURTLE_BODY_PARTS should only contain the characters 'L', 'E', and ' '")
+
+
+def main():
+
+    # Screen Setup
+    screen = turtle.Screen()
+    screen.setup(width=412, height=412)
+    screen.title('Spider')
+
+    # Pen Setup
+    pen = turtle.RawPen(screen, visible=False)
+    pen.speed('fastest')
+    pen.radians()
+
+    # Draw the Spider
+    draw_spider(pen)
+
+    # Mainloop until the window is clicked
+    screen.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
